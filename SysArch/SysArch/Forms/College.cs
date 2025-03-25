0d16a03437e4eee5b8b87d8c32e16b5dcb01749f
LAPTOP-37GEEPRC\jaspe <jaspertabanag96@gmail.com>
2025-03-26 04:30:37 +0800

    -feat: added btnUpdate and btnDelete for update and delete data logic.

--- SysArch/SysArch/Forms/College.cs
+++ SysArch/SysArch/Forms/College.cs
@@ -42,9 +42,65 @@ namespace SysArch.Forms
             }
         }
 
-        private void CollegeForm_Load(object sender, EventArgs e)
+        private void btnUpdate_Click(object sender, EventArgs e)
         {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
 
+                if (row.IsNewRow ||
+                    row.Cells["id"].Value == null || row.Cells["college_name"].Value == null ||
+                    row.Cells["college_code"].Value == null || row.Cells["is_active"].Value == null)
+                {
+                    MessageBox.Show("Cannot update an empty row.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                int id = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["id"].Value);
+                string collegeName = Convert.ToString(dataGridView1.Rows[rowIndex].Cells["college_name"].Value);
+                string collegeCode = Convert.ToString(dataGridView1.Rows[rowIndex].Cells["college_code"].Value);
+                int isActive = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["is_active"].Value);
+
+                UpdateCollegeModal updateCollege = new UpdateCollegeModal();
+                updateCollege.PopulateFields(id, collegeName, collegeCode, isActive);
+
+                updateCollege.Show();
+                this.Hide();
+
+                College_Load(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to update.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+            if (row.IsNewRow ||
+                row.Cells["id"].Value == null || row.Cells["college_name"].Value == null ||
+                row.Cells["college_code"].Value == null || row.Cells["is_active"].Value == null)
+            {
+                MessageBox.Show("Cannot delete an empty row.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var res = MessageBox.Show("Are you sure you want to delete this record?", "Delete Record", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (res == DialogResult.Yes)
+            {
+                int id = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["id"].Value);
+
+                CollegeService.DeleteCollege(id);
+
+                MessageBox.Show("College deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                College_Load(sender, e);
+            }
+        }
+
     }
 }
