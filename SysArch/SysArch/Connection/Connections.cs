1d5f0f92af7999bb4c463044df99ba6fdb48f23c
m4yoNICE <cenizaroswellrey@gmail.com>
2025-03-26 03:14:24 +0800

    -feat: added connection string

--- SysArch/SysArch/Connection/Connections.cs
+++ SysArch/SysArch/Connection/Connections.cs
@@ -11,6 +11,21 @@ namespace SysArch.Connection
 {
     public class Connections
     {
+        public static SqlConnection conn;
+        public static string dbConnect = "Server=LAPTOP-37GEEPRC;Database=CollegeMatrix;User Id=sa;Password=root";
 
+        public static void DB()
+        {
+            try
+            {
+                conn = new SqlConnection(dbConnect);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
