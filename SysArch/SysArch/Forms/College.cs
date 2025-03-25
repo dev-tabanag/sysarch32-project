43b6b08254c06f297beccf83a17e32a820eeb93e
LAPTOP-37GEEPRC\jaspe <jaspertabanag96@gmail.com>
2025-03-26 04:19:54 +0800

    -feat: added btn add and college load into my college form.

--- SysArch/SysArch/Forms/College.cs
+++ SysArch/SysArch/Forms/College.cs
@@ -20,6 +20,31 @@ namespace SysArch.Forms
             InitializeComponent();
         }
 
-       
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddCollegeModal college = new AddCollegeModal();
+            college.Show();
+            this.Hide();
+        }
+
+        private void College_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                CollegeService.ViewCollege(dataGridView1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading college:\n{ex.Message}",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void CollegeForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
